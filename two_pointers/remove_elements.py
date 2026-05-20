69cd51c039dcc8000b80cffda87457503a167493
PranaliBhoir <pranali.apps@gmail.com>
2026-05-20 23:34:08 +0530

    Two pointers added

---
+++ two_pointers/remove_elements.py
@@ -0,0 +1,8 @@
+def removeElement(nums,val):
+    left = 0
+    for right in range(len(nums)):
+        if nums[right] != val:
+            nums[left] = nums[right]
+            left += 1
+    return left
+print(removeElement([3,2,2,3],3))
