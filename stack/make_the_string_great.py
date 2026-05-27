5e615fb52052a9dc81c4af9629ef598ef3e29fad
PranaliBhoir <pranali.apps@gmail.com>
2026-05-27 23:42:05 +0530

    Stack - make string great added

---
+++ stack/make_the_string_great.py
@@ -0,0 +1,13 @@
+class Solution:
+    def makeGood(self, s):
+        stack = []
+        for ch in s:
+            if stack and abs(ord(stack[-1]) - ord(ch)) == 32:
+                stack.pop()
+            else:
+                stack.append(ch)
+        return "".join(stack)
+    
+s = "leEeetcode"
+sol = Solution()
+print(sol.makeGood(s))
