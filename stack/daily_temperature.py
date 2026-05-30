c270243a91cdc264e3e5dff19d95ca4d734cdd93
PranaliBhoir <pranali.apps@gmail.com>
2026-05-30 23:49:21 +0530

    Stack - daily temperature added

---
+++ stack/daily_temperature.py
@@ -0,0 +1,14 @@
+class Solution:
+    def dailyTemperatures(self, temperatures):
+        n = len(temperatures)
+        answer = [0] * n
+        stack = []
+        for i in range(n):
+            while stack and temperatures[i] > temperatures[stack[-1]]:
+                idx = stack.pop()
+                answer[idx] = i - idx
+            stack.append(i)
+        return answer
+temperatures = [73,74,75,71,69,72,76,73]
+sol = Solution()
+print(sol.dailyTemperatures(temperatures))
